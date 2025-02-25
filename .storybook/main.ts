e94e8980e96bb82b9f8874a1b2b95c1af3159ac5
peterdinis <petter1@pobox.sk>
2025-02-25 18:55:02 +0100

    feat: Initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/sveltekit';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|ts|svelte)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-svelte-csf",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/sveltekit",
+    "options": {}
+  }
+};
+export default config;
