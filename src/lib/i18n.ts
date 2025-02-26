71c449c8b93404274773636d7bac6bb1d7c47b23
peterdinis <petter1@pobox.sk>
2025-02-26 18:56:10 +0100

    infra: Adding i18n WIP

---
+++ src/lib/i18n.ts
@@ -0,0 +1,17 @@
+import { addMessages, init, locale } from 'svelte-i18n';
+
+// Import translation files
+import en from './locales/en.json';
+import sk from './locales/sk.json';
+
+export const setupI18n = () => {
+  addMessages('en', en);
+  addMessages('sk', sk);
+
+  init({
+    fallbackLocale: 'en',
+    initialLocale: localStorage.getItem('lang') || 'en'
+  });
+};
+
+export { locale };
