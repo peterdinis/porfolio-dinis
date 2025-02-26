003d058ee1cacaca0eba3665b4af382f12e05e73
peterdinis <petter1@pobox.sk>
2025-02-26 18:40:21 +0100

    feat: Adding scroll to top component

---
+++ src/lib/components/shared/ScrollToTop.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	export let showOnPx: number = 150;
+	let hidden: boolean = true;
+
+	function goTop(): void {
+		document.body.scrollIntoView({ behavior: 'smooth' });
+	}
+
+	function scrollContainer(): HTMLElement | null {
+		return document.documentElement || document.body;
+	}
+
+	function handleOnScroll(): void {
+		const container = scrollContainer();
+		if (!container) return;
+
+		hidden = container.scrollTop <= showOnPx;
+	}
+</script>
+
+<svelte:window on:scroll={handleOnScroll} />
+
+<div
+	class="fixed right-5 bottom-5 z-50 cursor-pointer rounded-full bg-black p-3 text-white transition-opacity duration-500 hover:bg-gray-700"
+	class:hidden
+	on:click={goTop}
+>
+	Back to top
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+	import ScrollToTop from '$lib/components/shared/ScrollToTop.svelte';
 	let { children } = $props();
 </script>
 
 {@render children()}
 <ModeWatcher />
+<ScrollToTop />
