3c199b75b368ae44f801e011c7283f483ba6eac1
peterdinis <petter1@pobox.sk>
2025-02-26 19:19:16 +0100

    fix: Fixing localStorage issue

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,4 +1,5 @@
 import { addMessages, init, locale } from 'svelte-i18n';
+import { browser } from '$app/environment';
 
 import en from './locales/en.json';
 import sk from './locales/sk.json';
@@ -7,9 +8,10 @@ export const setupI18n = () => {
   addMessages('en', en);
   addMessages('sk', sk);
 
+  const savedLocale = browser ? localStorage.getItem('lang') : null; 
   init({
     fallbackLocale: 'en',
-    initialLocale: localStorage.getItem('lang') || 'en'
+    initialLocale: savedLocale || 'en'
   });
 };
 
